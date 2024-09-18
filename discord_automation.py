67e0d4a61b14fc58ddb482cb0cc7b75b924d990a
IAmTechnologyMad <kenntshine@gmail.com>
2024-09-18 15:46:46 +0530

    Update discord_automation.py

--- discord_automation.py
+++ discord_automation.py
@@ -11,23 +11,26 @@ root.withdraw()
 print("[bold green] WELCOME TO RAPID FREELANCIN SCRAP SERVICE [/bold green]")
 print("[bold white] ----------------------------------------- [/bold white]\n")
 
-file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
+file_paths = filedialog.askopenfilenames(filetypes=[("JSON files", "*.json")])
 
-file_name = os.path.splitext(os.path.basename(file_path))[0]
+for file_path in file_paths:
+    file_name = os.path.splitext(os.path.basename(file_path))[0]
 
-with open(file_path, 'r', encoding='utf-8') as file:
-    data = json.load(file)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
     
-print(f"File chosen: [bold yellow]{file_name}.json[/bold yellow]\n")
+    print(f"File chosen: [bold yellow]{file_name}.json[/bold yellow]\n")
 
-n = [
-    (message['author']['name'], message['timestamp'], message['content'])
-    for message in data['messages'] if 'name' in message['author']
-]
+    n = [
+        (message['author']['name'], message['timestamp'], message['content'])
+        for message in data['messages'] if 'name' in message['author']
+    ]
 
-output_file = f"{file_name}.txt"
-with open(output_file, 'w', encoding='utf-8') as file:
-    for name, timestamp, content in tqdm(n, colour="red"):
-        file.write(f"Nickname: {name}\nTimestamp: {timestamp}\nMessage: {content}\n\n")
+    output_file = f"{file_name}.txt"
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for name, timestamp, content in tqdm(n, desc=f"Processing {file_name}", colour="red"):
+            file.write(f"Nickname: {name}\nTimestamp: {timestamp}\nMessage: {content}\n\n")
 
-print(f"\n[bold yellow] File saved as {output_file} [/bold yellow]")
+    print(f"\n[bold yellow] File saved as {output_file} [/bold yellow]\n")
+    
+print("[bold green] CODE EXIT HERE, THANKYOU! [/bold green]")
